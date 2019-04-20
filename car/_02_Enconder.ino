0effcd215ef8bdddc22a81d2fbd6ef92470b19f9
Leo Hsu <39806753+leohsu870325@users.noreply.github.com>
2019-04-20 12:04:59 +0800

    Revise the mistake about data type of mSamplingTime.

--- car/_02_Enconder.ino
+++ car/_02_Enconder.ino
@@ -6,8 +6,8 @@ void SetupEncoder(){
     motor_A.SetEncoderPins(INTA, DIRA);
     motor_B.SetEncoderPins(INTB, DIRB);
     
-    motor_A.SetSamplingTime(dT);
-    motor_B.SetSamplingTime(dT);
+    motor_A.SetSamplingTime(0.05);
+    motor_B.SetSamplingTime(0.05);
 
     attachInterrupt(digitalPinToInterrupt(motor_A.GetEncoderInterruptPin()), Encoder_A_Interrupt, RISING);
     attachInterrupt(digitalPinToInterrupt(motor_B.GetEncoderInterruptPin()), Encoder_B_Interrupt, RISING);
