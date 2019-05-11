d2587b4aeb55064c63c39f38edd3b634e80b6d43
innnnn <innnnn.eed05@g2.nctu.edu.tw>
2019-05-11 18:43:49 +0800

    fix the fragment crash problem, add carState, JoystickControl, StatefeedbackControl fragment

--- app/app/src/main/java/com/example/self_balancebotapplication/MainActivity.java
+++ app/app/src/main/java/com/example/self_balancebotapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.self_balancebotapplication;
 
+import android.app.FragmentTransaction;
 import android.content.pm.ActivityInfo;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -16,10 +18,16 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Toolbar toolbar;
     private DrawerLayout drawer;
     private HomeFragment homeFragment = new HomeFragment();
     private BluetoothFragment bluetoothFragment = new BluetoothFragment();
+    private JoystickControlFragment joystickControlFragment = new JoystickControlFragment();
     private PIDControlFragment pidControlFragment = new PIDControlFragment();
+    private StatefeedbackControlFragment statefeedbackControlFragment = new StatefeedbackControlFragment();
+    private CarStateFragment carStateFragment = new CarStateFragment();
+    private Fragment lastFragment;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         bluetoothFragment.setActivity(this);
 
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
@@ -44,25 +52,54 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        if(savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
+        // set the homeFragment as first fragment
+        if(!homeFragment.isAdded()){
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, homeFragment).commit();
+            getSupportFragmentManager().beginTransaction().show(homeFragment).commit();
             navigationView.setCheckedItem(R.id.nav_home);
+            lastFragment = homeFragment;
         }
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) { // which fragment is selected
         int id = menuItem.getItemId();
         switch(id){
             case R.id.nav_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
+                currentFragment = homeFragment;
                 break;
             case R.id.nav_bluetooth:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, bluetoothFragment).commit();
+                currentFragment = bluetoothFragment;
                 break;
-            case R.id.nav_pidcontrol:
-                //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, pidControlFragment).commit();
+            case R.id.nav_joystick_control:
+                currentFragment = joystickControlFragment;
                 break;
+            case R.id.nav_pid_control:
+                currentFragment = pidControlFragment;
+                break;
+            case R.id.nav_statefeedback_control:
+                currentFragment = statefeedbackControlFragment;
+                break;
+            case R.id.nav_car_state:
+                currentFragment = carStateFragment;
+                break;
+        }
+
+        // use hide and show , instead of replace
+        // hide, show: does not destroy fragment
+        // replace: destroy old fragment and create an new fragment
+        if(currentFragment != lastFragment){
+            if(!currentFragment.isAdded()){
+                getSupportFragmentManager().beginTransaction().hide(lastFragment).commit();
+                getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, currentFragment).commit();
+                getSupportFragmentManager().beginTransaction().show(currentFragment).commit();
+                System.out.println("1");
+            } else {
+                getSupportFragmentManager().beginTransaction().hide(lastFragment).commit();
+                getSupportFragmentManager().beginTransaction().show(currentFragment).commit();
+                System.out.println("2");
+            }
+            lastFragment = currentFragment;
         }
 
         drawer.closeDrawer(GravityCompat.START);
